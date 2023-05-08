807fb43e881169ba5facec82717214cd363de3f4
Oluwafikayo Ajayi <oluwafikayoajayi@gmail.com>
2023-05-08 15:18:07 -0400

    added liniting, env, optimise prod build

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import 'main.css';
 
+let ENV = import.meta.env;
+
 function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold text-red-500 underline text-center">
-        Ijosin Web
+        {ENV.VITE_NAME}
       </h1>
 
     </div>
